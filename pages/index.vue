1adef34e3221db56da4638d28e12958767858dd8
tuna <ev0.kpx@gmail.com>
2023-09-15 14:38:57 +0900

    tailwind導入

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <h1>Index</h1>
+  <div class="w-screen h-screen bg-blue-500 flex justify-center items-center">
+    <h1 class="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+  </div>
 </template>
